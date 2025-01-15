0d26f178bf3e0b8df11f6a92318953228ec58d62
Carter Burn <899382+carterburn@users.noreply.github.com>
2025-01-14 19:18:19 -0500

    block identification for markdown

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 def markdown_to_blocks(markdown):
     sections = markdown.split("\n\n")
     blocks = []
@@ -7,3 +9,46 @@ def markdown_to_blocks(markdown):
         blocks.append(block.strip())
 
     return blocks
+
+def block_to_block_type(block):
+    # check to see if this block is just a heading
+    if block.startswith("#"):
+        return "heading"
+
+    # check if we start and end with ``` (if we START, but don't end, that is an
+    # error)
+    if block.startswith("```"):
+        if block.endswith("```"):
+            return "code"
+        else:
+            # we just return paragraph
+            return "paragraph"
+
+    # split up lines now to check each line has a quote, or list character
+    lines = block.split("\n")
+    quoted = list(filter(lambda x: x.startswith(">"), lines))
+    if len(quoted) == len(lines):
+        # we get everything back, meaning they all start with 
+        return "quote"
+
+    unordered_star = list(filter(lambda x: x.startswith("* "), lines))
+    unordered_dash = list(filter(lambda x: x.startswith("- "), lines))
+    if len(lines) == len(unordered_star) or len(lines) == len(unordered_dash) or len(lines) == (len(unordered_star) + len(unordered_dash)):
+        return "unordered_list"
+
+    ordered_list = list(map(lambda x: re.findall(r"(\d)\. (.*)", x), lines))
+    # check we have the right amount of items (number starts each line followed
+    # by . and space)
+    if len(lines) != len(ordered_list):
+        return "paragraph"
+    # check we increment, starting at 1
+    cnt = 0
+    for item in ordered_list:
+        if len(item) == 0:
+            return "paragraph"
+        if int(item[0][0]) != cnt + 1:
+            # didn't increment, paragraph
+            return "paragraph"
+        cnt += 1
+
+    return "ordered_list"
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import block_to_block_type, markdown_to_blocks
 
 class TestUtils(unittest.TestCase):
     def test_block_split(self):
@@ -60,6 +60,32 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+    def test_heading_block(self):
+        text = "# Heading"
+        self.assertEqual("heading", block_to_block_type(text))
+        text = "## Heading 2"
+        self.assertEqual("heading", block_to_block_type(text))
+
+    def test_code_block(self):
+        text = "```python\n\tx = 5\n\ty=10\n\tprint(x+y)\n```"
+        self.assertEqual("code", block_to_block_type(text))
+
+    def test_quote_block(self):
+        text = "> Quote line 1\n> Quote line 2"
+        self.assertEqual("quote", block_to_block_type(text))
+
+    def test_unordered_list(self):
+        text = "* List item\n* List item"
+        self.assertEqual("unordered_list", block_to_block_type(text))
+
+    def test_ordered_list(self):
+        text = "1. List item\n2. List item"
+        self.assertEqual("ordered_list", block_to_block_type(text))
+
+    def test_paragraph(self):
+        text = "Just a normal paragraph\n"
+        self.assertEqual("paragraph", block_to_block_type(text))
 
 if __name__ == "__main__":
     unittest.main()
